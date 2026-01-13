66bb7550ae2888eb432ae773d581d1009b171794
RaQshka <buba_q20@mail.ru>
2026-01-13 06:50:55 +0300

    fixed logic

--- Notes.Application/Notes/Queries/GetNoteList/NoteLookupDto.cs
+++ Notes.Application/Notes/Queries/GetNoteList/NoteLookupDto.cs
@@ -8,12 +8,14 @@ namespace Notes.Application.Notes.Queries.GetNoteList
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Note, NoteLookupDto>()
                 .ForMember(x => x.Id, opt => opt.MapFrom(note => note.Id))
-                .ForMember(x => x.Title, opt => opt.MapFrom(note => note.Title));
+                .ForMember(x => x.Title, opt => opt.MapFrom(note => note.Title))
+                .ForMember(x => x.CreatedAt, opt => opt.MapFrom(note => note.CreatedAt));
 
         }
     }
